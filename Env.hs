b08b29b28e219e92a86e77edeb03a346701c8007
Adam <adam@cthulahoops.org>
2014-01-26 23:41:01 +0000

    Env - allow memory to store pairs as well as frames.

--- Env.hs
+++ Env.hs
@@ -4,12 +4,12 @@ import qualified Data.Map as M
 
 type Ptr = Int
 
-data Frame k v = Frame (Maybe Ptr) (M.Map k v)
+data Cell k v = Pair v v | Frame (Maybe Ptr) (M.Map k v)
     deriving (Show)
 
 data Memory k v = Memory {
                     memoryNext :: Ptr,
-                    memory     :: M.Map Ptr (Frame k v)}
+                    memory     :: M.Map Ptr (Cell k v)}
     deriving (Show)
 
 data Env k v = Env {
@@ -26,20 +26,29 @@ newEnv = Env {envPtr = ptr, envMemory = mem}
 newFrame :: Maybe Ptr -> Memory k v -> (Ptr, Memory k v)
 newFrame parent (Memory next map) = (next, Memory {memoryNext = (next + 1), memory = (M.insert next (Frame parent M.empty) map)})
 
+newPair :: v -> v -> (Memory k v) -> (Ptr, Memory k v)
+newPair car cdr (Memory next map) = (next, Memory {memoryNext = (next + 1), memory = (M.insert next (Pair car cdr) map)})
+
+-- deref :: Ptr -> Memory k v -> Maybe v
+deref ptr mem = M.lookup ptr (memory mem)
+
 envStore  key value env = env { envMemory = store (envPtr env) key value (envMemory env) }
 envLookup key env = lookup (envPtr env) key (envMemory env)
+envGet    ptr env = M.lookup ptr (memory $ envMemory env)
 envSwitch ptr env = env {envPtr = ptr}
 envFrame  maybePtr env = (newPtr, env {envMemory = mem})
     where (newPtr, mem) = newFrame maybePtr (envMemory env)
+envPair   car cdr env = (newPtr, env {envMemory = mem})
+    where (newPtr, mem) = newPair car cdr mem
 
 store ptr key value (Memory next mem) = Memory next (M.insert ptr (Frame parent (M.insert key value map)) mem)
     where Just (Frame parent map) = M.lookup ptr mem
 
 lookup ptr key m@(Memory next mem) = case M.lookup ptr mem of
-                                            Just (Frame parent map) ->
-                                                 case M.lookup key map of 
-                                                     Just value -> Just value
-                                                     Nothing    -> case parent of
-                                                                        Just p' -> lookup p' key m
-                                                                        Nothing -> Nothing
+                                            Just frame -> lookupFrame frame
                                             Nothing -> Nothing
+    where lookupFrame (Frame parent map) = case M.lookup key map of 
+                                                 Just value -> Just value
+                                                 Nothing    -> case parent of
+                                                        Just p' -> lookup p' key m
+                                                        Nothing -> Nothing
