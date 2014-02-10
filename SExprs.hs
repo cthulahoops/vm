2fced4b07b4f243501b659ceabdb86dce9e2aa61
Adam <adam@cthulahoops.org>
2014-02-10 23:54:08 +0000

    SExprs - silly show instance for easy debugging of transforms.

--- SExprs.hs
+++ SExprs.hs
@@ -1,4 +1,15 @@
 module SExprs where
 
+import Data.List
+
 data SExpr = SList [SExpr] | SSymbol String | SInt Integer | SBool Bool | SString String
     deriving (Eq)
+
+instance Show SExpr where
+    show (SList xs) = "(" ++ concat (intersperse " " (map show xs)) ++ ")"
+    show (SSymbol x) = x
+    show (SInt x) = show x
+    show (SBool True) = "#t"
+    show (SBool False) = "#f"
+    show (SString x) = show x
+    
