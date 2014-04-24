ed9f535d057d92b122f459dee8a7a1c7407895b6
Adam <adam@cthulahoops.org>
2014-04-24 17:45:40 +0100

    Memory - clean up using Control.Lens

--- Memory.hs
+++ Memory.hs
@@ -1,6 +1,20 @@
-module Memory where
+{-# LANGUAGE TemplateHaskell #-}
+module Memory (
+    Memory,
+    Cell(..),
+    newFrame,
+    newMemory,
+    newPair,
+    deref,
+    lookup,
+    elems,
+    sweep,
+    store) where
 
 import Prelude hiding (lookup)
+import Control.Lens
+import Data.Maybe
+import Control.Applicative
 import qualified Data.Map as M
 import VmTypes
 
@@ -8,34 +22,43 @@ data Cell k v = Pair v v | Frame (Maybe Ptr) (M.Map k v)
     deriving (Show)
 
 data Memory k v = Memory {
-                    memoryNext :: Ptr,
-                    memory     :: M.Map Ptr (Cell k v)}
+                    _memoryNext :: Ptr,
+                    _memoryMap  :: M.Map Ptr (Cell k v)}
     deriving (Show)
+makeLenses ''Memory
 
 newMemory :: Memory k v
-newMemory = Memory {memoryNext = 0, memory = M.empty}
+newMemory = Memory {
+    _memoryNext = 0,
+    _memoryMap  = M.empty}
 
 newFrame :: Maybe Ptr -> Memory k v -> (Ptr, Memory k v)
-newFrame parent (Memory next map) = (next, Memory {memoryNext = (next + 1), memory = (M.insert next (Frame parent M.empty) map)})
+newFrame parent memory = insert (Frame parent M.empty) memory
 
 newPair :: v -> v -> (Memory k v) -> (Ptr, Memory k v)
-newPair car cdr (Memory next map) = (next, Memory {memoryNext = (next + 1), memory = (M.insert next (Pair car cdr) map)})
+newPair car cdr memory = insert (Pair car cdr) memory
 
--- deref :: Ptr -> Memory k v -> Maybe v
-deref ptr mem = M.lookup ptr (memory mem)
+insert :: Cell k v -> Memory k v -> (Ptr, Memory k v)
+insert value memory = (next, over memoryNext (+1) $ over memoryMap (M.insert next value) $ memory)
+    where next = memory ^. memoryNext
+
+deref :: Ptr -> Memory k v -> Maybe (Cell k v)
+deref ptr mem = M.lookup ptr (mem ^. memoryMap)
 
 store ptr key value (Memory next mem) = Memory next (M.insert ptr (Frame parent (M.insert key value map)) mem)
     where Just (Frame parent map) = M.lookup ptr mem
 
-lookup ptr key m@(Memory next mem) = case M.lookup ptr mem of
-                                            Just frame -> lookupFrame frame
-                                            Nothing -> Nothing
-    where lookupFrame (Frame parent map) = case M.lookup key map of 
-                                                 Just value -> Just value
-                                                 Nothing    -> case parent of
-                                                        Just p' -> lookup p' key m
-                                                        Nothing -> Nothing
-          lookupFrame pair = error $ "VM Error: Expected pair got frame: " ++ show pair
+lookup ptr key memory = firstJust $ M.lookup key <$> frameList ptr memory
+
+frameList ptr memory = case M.lookup ptr (memory ^. memoryMap) of
+    Nothing                        -> []
+    Just (Frame (Just parent) map) -> map : frameList parent memory
+    Just (Frame Nothing map)       -> [map]
+    Just pair                      -> error $ "Memory Error: Expected pair got frame: " ++ show pair
+
+firstJust :: [Maybe a] -> Maybe a
+firstJust = listToMaybe . catMaybes
+
 elems = M.elems
 
 sweep :: [Ptr] -> Memory k a -> Memory k a
